fb9406b1a2c3c272755f52165e96227643d229ed
bwbarron <brandon.barron@live.com>
2015-10-13 17:14:01 -0700

    javascript stuff

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,25 @@
-/**
- * application script for index.html
- */
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    function forEachElement(collection, fn) {
+        for (var i = 0; i < collection.length; i++) {
+            fn(collection[i]);
+        }
+    }
+
+    document.getElementById("click-me").addEventListener("click", function() {
+        var alerts = document.querySelectorAll(".alert");
+        forEachElement(alerts, function(alert) {
+            alert.style.display = "block";
+        });
+    });
+
+    var closeButtons = document.querySelectorAll(".alert .close");
+    forEachElement(closeButtons, function(button) {
+        button.addEventListener("click", function() {
+            button.parentElement.style.display = "none";
+        });
+    });
+
+});
--- javascript/js/states.js
+++ javascript/js/states.js
@@ -1,3 +1,23 @@
-/**
- * application script for the states.html
- */
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    function addStates(states, listElement) {
+        listElement.innerHTML = "";
+        states.forEach(function(state) {
+            var li = document.createElement("li");
+            li.textContent = state.name;
+            listElement.appendChild(li);
+        });
+    }
+
+    var statesList = document.getElementById("states-list")
+    addStates(usaStates, statesList);
+
+    document.getElementById("state-filter-field").addEventListener("keyup", function() {
+        var filter = this.value;
+        var filteredStates = usaStates.filter(function(state) {
+            return state.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0;
+        });
+        addStates(filteredStates, statesList);
+    });
+});
