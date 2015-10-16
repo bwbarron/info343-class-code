4c71afb5979c8bb74d210bf88f4146766afa406c
bwbarron <brandon.barron@live.com>
2015-10-15 17:12:04 -0700

    jQuery and leaflet

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,19 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+"use strict";
+
+$(function() {
+    $("a").attr("target", "_blank");
+    $("article").hide().fadeIn();
+    $("#toggle-article").click(function() {
+        $("article").fadeToggle();
+    });
+
+    var url = "http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f";
+    $.getJSON(url).then(function(data) {
+        console.log(data);
+        var temp = data.main.temp;
+        $("#temp").text(Math.round(temp));
+    });
+});
