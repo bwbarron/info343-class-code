90b9defddcc300dc74ec6b7ba55829d64bff7b58
bwbarron <brandon.barron@live.com>
2015-10-22 16:50:30 -0700

    bleh

--- parse/js/app.js
+++ parse/js/app.js
@@ -1,7 +1,7 @@
 // script for index.html
 
 Parse.initialize("gEp15mUlnA8xNOZDmuxKqSD0eEYNuNrdT1sZ7prT", "R8mX254MS70DE7XzJ4j0wvsV3CFglVlG419bXyz8");
-// jessie's
+// jessie's parse.com keys
 //Parse.initialize("eU9YJ9oNpzfeEondOnXdFr7ZkyrpwA7l3zeIEyjl", "YphdeeP7rTbVupkSiWU50nWPFfqtioi52yG74roA");
 
 
@@ -13,10 +13,12 @@ $(function() {
     // new query that will return all tasks ordered by their createdAt field
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending("createdAt");
+    tasksQuery.notEqualTo("done", true);
 
     var tasksList = $("#tasks-list");
     var errorMessage = $("#error-message");
     var tasks = [];
+    var ratingElement = $("#rating");
 
     function displayError(err) {
         errorMessage.text(err.message);
@@ -50,11 +52,32 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement("li"))
+            var li = $(document.createElement("li"))
                 .text(task.get("title"))
-                .appendTo(tasksList);
+                .addClass(task.get("done") ? "completed-task" : "")
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set("done", !task.get("done"));
+                    task.save().then(renderTasks, displayError);
+                });
+
+            $(document.createElement("span"))
+                .raty({
+                        readOnly: true,
+                        score: (task.get("rating") || 0),
+                        hints: ["crap", "awful", "ok", "nice", "awesome"]
+                    })
+                .appendTo(li);
         });
     }
+
+    function showMessage(message) {
+        // boolean operations like this can be used for setting defaults
+        // depends on truthy or falsey values
+        alert(message || "Hello");
+    }
+
+    //showMessage(); // this would cause an alert with "Hello" to appear
 
     $("#new-task-form").submit(function(event) {
         event.preventDefault();
@@ -63,14 +86,20 @@ $(function() {
         var title = titleInput.val();
         var task = new Task();
         task.set("title", title);
-        task.save().then(fetchTasks, displayError).then(function() {
-            titleInput.val("");
-        });
+        task.set("rating", ratingElement.raty("score"));
+        task.save()
+            .then(fetchTasks, displayError)
+            .then(function() {
+                titleInput.val("");
+                ratingElement.raty("set", {});
+            });
 
         return false;
     });
 
     fetchTasks();
+
+    ratingElement.raty();
 
     window.setInterval(fetchTasks, 3000);
 });
