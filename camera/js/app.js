5afe7a98bbe2453d9c1b58a6bab1f2dcdbbfa3d9
bwbarron <brandon.barron@live.com>
2015-11-19 17:06:13 -0800

    blah

--- camera/js/app.js
+++ camera/js/app.js
@@ -2,6 +2,55 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    var video = document.querySelector("video");
+    var canvas = document.querySelector("canvas");
+    var snapshot = document.querySelector("img");
+    var context = canvas.getContext("2d");
+    var videoStream;
+    var mouseIsDown = false;
+
+    navigator.getUserMedia({video: true/*, audio: true*/}, function (stream) {
+        videoStream = stream;
+        video.src = window.URL.createObjectURL(stream);
+    }, function (error) {console.error(error);});
+
+    video.addEventListener("click", function () {
+        if (videoStream) {
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            context.drawImage(video, 0, 0);
+        }
+    });
+
+    canvas.addEventListener("mousedown", function (event) {
+        var x = event.clientX - canvas.offsetLeft;
+        var y = event.clientY - canvas.offsetTop + window.scrollY;
+        context.beginPath();
+        context.moveTo(x, y);
+        mouseIsDown = true;
+    });
+
+    canvas.addEventListener("mousemove", function (event) {
+        var x = event.clientX - canvas.offsetLeft;
+        var y = event.clientY - canvas.offsetTop + window.scrollY;
+        if (mouseIsDown) {
+            context.lineTo(x, y);
+            context.stroke();
+        }
+    });
+
+    canvas.addEventListener("mouseup", function (event) {
+        mouseIsDown = false;
+    });
+
+    document.querySelector("#btn-snapshot").addEventListener("click", function () {
+        snapshot.src = canvas.toDataURL();
+    });
 
 });
 
--- notifications/js/app.js
+++ notifications/js/app.js
@@ -3,4 +3,34 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    function askPermission() {
+        Notification.requestPermission(function (result) {
+            if (result === "granted") {
+                showNotification("Thanks bitch", "Now you will see notifications");
+            }
+        });
+    }
+
+    function showNotification(title, body) {
+        var notification = new Notification(title, {
+            body: body,
+            icon: "img/notification.png"
+        });
+        window.setTimeout(notification.close.bind(notification), 3000);
+    }
+
+    var triggerBtn = document.getElementById(("trigger"));
+    triggerBtn.addEventListener("click", function () {
+        switch(Notification.permission) {
+            case "granted":
+                showNotification("Hello", "triggered at " + new Date().toLocaleTimeString());
+                break;
+            case "denied":
+                alert("Please enable notifications");
+                break;
+            default:
+                askPermission();
+        }
+    });
+
 });
